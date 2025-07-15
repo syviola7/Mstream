b72898470afc48a70cacfaa2ff7e4920383b583b
Sy <rhykoviola017@gmail.com>
2025-07-15 23:15:06 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["image.tmdb.org", "encrypted-tbn0.gstatic.com"],
+  },
+};
+
+export default nextConfig;
